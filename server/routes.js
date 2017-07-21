5a666b4bbb24d3df8b7bd3f0da2a7f6d8ff1f9e4
craigpullar <craigmichaelpullar@gmail.com>
2017-07-21 05:57:07 +0100

    release 1.3.0

--- server/routes.js
+++ server/routes.js
@@ -1,28 +1,39 @@
 let routes = require('./models/Routes');
+var jwt = require('jsonwebtoken');
+let secret = require('./secret.json');
 // let db = require('./database');
 // db.connect('localhost', 28015, '{admin_pass}', 'doMore');
 
 
-//Pages Routes
-routes.addRoute('GET','/', (request,reply) => {
-	var data = {
-		title: 'This is Index!',
-		message: 'Hello, World. You crazy handlebars layout'
-	};
-	return reply.view('index', data);
+//API Routes
+routes.addRoute('GET','/api/v0.1', (request, reply) => {
+	let data = {
+		"API Version" : "0.1",
+		"Date Created" : "21/07/17"
+	}
+	reply(data);
 });
+routes.addRoute('POST','/api/v0.1/getToken', (request, reply) => {
+// sign with default (HMAC SHA256)
+
+//Verify secret against DB of devices
+let client_secret = request.params.client_secret;
+
+//Verify User & pass against DB of users
+let username = request.param.username;
+let password = request.param.password;
 
 
-//Resource Routes
-routes.addRoute('GET','/js/{filename}.js', (request,reply) => {
-	reply.file(`./client/js/${request.params.filename}.js`);
-});
-routes.addRoute('GET', '/css/{filename}.css', (request,reply) => {
-	reply.file(`./client/css/${request.params.filename}.css`);
-});
-routes.addRoute('GET', '/images/{filename}', (request,reply) => {
-	reply.file(`./client/images/${request.params.filename}`);
-});
+// sign with RSA SHA256
+var cert = secret.private_key; // get private key
+var token = jwt.sign({ deviceID: '1' }, cert);
+let data = {
+	"token" : token
+};
 
+reply(data);
+
+});
 
 module.exports = routes.getRoutes();
+
